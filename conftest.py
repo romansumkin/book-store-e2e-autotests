d1357ded686303f00494202608a289ea240a1e12
romansumkin <sumkin.romanw@gmail.com>
2023-06-02 19:16:16 +0400

    Add login link is present test

--- conftest.py
+++ conftest.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en', help='Choose language')
 
-
 @pytest.fixture(scope="function")
 def browser(request):
 
